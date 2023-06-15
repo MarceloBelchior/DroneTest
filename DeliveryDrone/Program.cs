e3fda9985f5743f5ee0467fbd799b02c7a9105d8
Marcelo <marcelo.belchior@gmail.com>
2023-06-14 21:45:52 -0300

    Initial Commit

---
+++ DeliveryDrone/Program.cs
@@ -0,0 +1,111 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace DroneDelivery
+{
+
+
+
+   public class Program
+    {
+
+
+
+        public static void Main(string[] args)
+        {
+            string inputFilePath = "input.txt";
+            string outputFilePath = "output.txt";
+
+            // Step 1: Parse the input data
+            string[] lines = File.ReadAllLines(inputFilePath);
+
+            string[] drones = lines[0].Split(',').Select(x => x.Trim()).ToArray();
+            List<Tuple<string, int>> dtuple = new List<Tuple<string, int>>();
+            //Step 2: Get the drone capacity
+            for (int i = 0; i < drones.Length; i++)
+            {
+                int capacity = 0;
+                if (drones[i].Contains("Drone") && int.TryParse(ReplaceSquareBrackets(drones[i + 1]), out capacity))
+                {
+                    dtuple.Add(ConvertToTuple(ReplaceSquareBrackets(drones[i]), capacity));
+                }
+
+            }
+
+
+            string[][] locations = lines.Skip(1).Select(line => line.Split(',').Select(x => x.Trim()).ToArray()).ToArray();
+            List<Tuple<string, int>> ltuple = new List<Tuple<string, int>>();
+            //step 3: Get the location capacity
+            for (int i = 0; i < locations.Length; i++)
+            {
+                int capacity = 0;
+                if (locations[i][0].Contains("Location") && int.TryParse(ReplaceSquareBrackets(locations[i][1]), out capacity))
+                {
+                    ltuple.Add(ConvertToTuple(ReplaceSquareBrackets(locations[i][0]), capacity));
+                }
+
+            }
+
+            // Step 4: Create a dictionary to track deliveries for each drone
+
+            Dictionary<string, List<string>> deliveries = new Dictionary<string, List<string>>();
+
+            // Step 5: Assign locations to drones
+            ltuple.OrderByDescending(c => c.Item2).ToList().ForEach(y =>
+            {
+                for (int i = 0; i < dtuple.Count; i++)
+                {
+                    if (dtuple[i].Item2 >= y.Item2)
+                    {
+                        if (!deliveries.ContainsKey(dtuple[i].Item1))
+                        {
+                            deliveries.Add(dtuple[i].Item1, new List<string>());
+                        }
+                        deliveries[dtuple[i].Item1].Add(y.Item1);
+                        dtuple[i] = Tuple.Create(dtuple[i].Item1, dtuple[i].Item2 - y.Item2);
+                        break;
+                    }
+                }
+            });
+
+            // Step 6: Generate the output file
+            using (StreamWriter writer = new StreamWriter(outputFilePath))
+            {
+                foreach (string drone in dtuple.Select(x => x.Item1))
+                {
+                    writer.WriteLine($"[{drone}]");
+                    if (deliveries.ContainsKey(drone))
+                    {
+
+                        List<string> trips = deliveries[drone];
+                        int vp = 0;
+                        foreach (var item in trips)
+                        {
+                            writer.WriteLine($"Trip #{vp + 1}");
+                            writer.WriteLine(string.Join(", ", "[" + item + "]"));
+                            vp++;
+                        }
+                    }
+
+                }
+            }
+        }
+        //utils to remove square brackets
+        static string ReplaceSquareBrackets(string input)
+        {
+            string pattern = @"\[|\]";
+            string replacement = "";
+            return Regex.Replace(input, pattern, replacement);
+        }
+        //utils to convert to tuple
+        static Tuple<string, int> ConvertToTuple(string item, int capacity)
+        {
+            return Tuple.Create(ReplaceSquareBrackets(item), capacity);
+        }
+
+
+    }
+}
---
+++ TestDeliveryDrone/UnitTest1.cs
@@ -0,0 +1,92 @@
+﻿namespace test_drone_delivery
+{
+
+
+    [TestFixture]
+    public class DroneDeliveryTests
+    {
+        private string inputFilePath = "input.txt";
+        private string outputFilePath = "output.txt";
+
+        [SetUp]
+        public void SetUp()
+        {
+            // Create a sample input file
+            string[] inputLines = new string[]
+            {
+                "[Drone1],[20], [Drone2 Beta], [250], [Drone3], [100]",
+                "[LocationA], [200]",
+                "[Locationb], [20]",
+                "[Locationc], [10]",
+                "[Locationd], [150]",
+                "[Locatione], [220]",
+                "[Locationf], [210]",
+                "[Locationg], [20]",
+                "[Locationh], [2]",
+                "[Locationgx], [20]",
+                "[Locationhs], [2]"
+            };
+
+            File.WriteAllLines(inputFilePath, inputLines);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            // Delete the input and output files after each test
+            if (File.Exists(inputFilePath))
+                File.Delete(inputFilePath);
+
+            if (File.Exists(outputFilePath))
+                File.Delete(outputFilePath);
+        }
+
+        [Test]
+        public void AssignDeliveries_WithValidInput_AssignsDeliveriesToDrones()
+        {
+            // Arrange
+            DroneDelivery.Program.Main(null);
+
+            // Act
+            string[] outputLines = File.ReadAllLines(outputFilePath);
+
+            // Assert
+            string[] expectedOutputLines = new string[]
+            {
+                "[Drone1]",
+                "Trip #1",
+                "[Locationb]",
+                "[Drone2 Beta]",
+                "Trip #1",
+                "[Locatione]",
+                "Trip #2",
+                "[Locationg]",
+                "Trip #3",
+                "[Locationc]",
+                "[Drone3]",
+                "Trip #1",
+                "[Locationgx]",
+                "Trip #2",
+                "[Locationh]",
+                "Trip #3",
+                "[Locationhs]"
+            };
+
+            CollectionAssert.AreEqual(expectedOutputLines, outputLines);
+        }
+
+        [Test]
+        public void AssignDeliveries_WithInvalidInput_ReturnsEmptyOutput()
+        {
+            // Arrange
+            File.WriteAllText(inputFilePath, "Invalid input");
+
+            // Act
+            DroneDelivery.Program.Main(null);
+
+            // Assert
+            string[] outputLines = File.ReadAllLines(outputFilePath);
+            Assert.IsEmpty(outputLines);
+        }
+    }
+}
